bc1897c5e171ef47c6f922e5e9dd2fc47b1ce1c4
victorcheney <victorcheney@163.com>
2018-11-26 23:30:48 +0800

    螺旋时间线图形螺旋曲线上显示随时间变化的数

--- examples/condegramSpiralPlot/index.js
+++ examples/condegramSpiralPlot/index.js
@@ -1,54 +1,54 @@
-var width = 500,
-  height = 500,
-  start = 0,
-  end = 2.25,
-  numSpirals = 3
-margin = {
+let width = 500;
+let height = 500;
+let start = 0;
+let end = 2.25;
+let numSpirals = 3;
+let margin = {
   top: 50,
   bottom: 50,
   left: 50,
   right: 50
 };
 
-var theta = function (r) {
+let theta = r => {
   return numSpirals * Math.PI * r;
 };
 
 // used to assign nodes color by group
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+let color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var r = d3.min([width, height]) / 2 - 40;
+let r = d3.min([width, height]) / 2 - 40;
 
-var radius = d3.scaleLinear()
+let radius = d3.scaleLinear()
   .domain([start, end])
   .range([40, r]);
 
-var svg = d3.select("#chart").append("svg")
+let svg = d3.select("#chart").append("svg")
   .attr("width", width + margin.right + margin.left)
   .attr("height", height + margin.left + margin.right)
   .append("g")
   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var points = d3.range(start, end + 0.001, (end - start) / 1000);
+let points = d3.range(start, end + 0.001, (end - start) / 1000);
 
-var spiral = d3.radialLine()
+let spiral = d3.radialLine()
   .curve(d3.curveCardinal)
   .angle(theta)
   .radius(radius);
 
-var path = svg.append("path")
+let path = svg.append("path")
   .datum(points)
   .attr("id", "spiral")
   .attr("d", spiral)
   .style("fill", "none")
   .style("stroke", "steelblue");
 
-var spiralLength = path.node().getTotalLength(),
-  N = 365,
-  barWidth = (spiralLength / N) - 1;
-var someData = [];
-for (var i = 0; i < N; i++) {
-  var currentDate = new Date();
+let spiralLength = path.node().getTotalLength();
+let N = 365;
+let barWidth = (spiralLength / N) - 1;
+let someData = [];
+for (let i = 0; i < N; i++) {
+  let currentDate = new Date();
   currentDate.setDate(currentDate.getDate() + i);
   someData.push({
     date: currentDate,
@@ -57,15 +57,15 @@ for (var i = 0; i < N; i++) {
   });
 }
 
-var timeScale = d3.scaleTime()
-  .domain(d3.extent(someData, function (d) {
+let timeScale = d3.scaleTime()
+  .domain(d3.extent(someData, d => {
     return d.date;
   }))
   .range([0, spiralLength]);
 
 // yScale for the bar height
-var yScale = d3.scaleLinear()
-  .domain([0, d3.max(someData, function (d) {
+let yScale = d3.scaleLinear()
+  .domain([0, d3.max(someData, d => {
     return d.value;
   })])
   .range([0, (r / numSpirals) - 30]);
@@ -74,9 +74,9 @@ svg.selectAll("rect")
   .data(someData)
   .enter()
   .append("rect")
-  .attr("x", function (d, i) {
+  .attr("x", (d, i) => {
 
-    var linePer = timeScale(d.date),
+    let linePer = timeScale(d.date),
       posOnLine = path.node().getPointAtLength(linePer),
       angleOnLine = path.node().getPointAtLength(linePer - barWidth);
 
@@ -88,25 +88,25 @@ svg.selectAll("rect")
 
     return d.x;
   })
-  .attr("y", function (d) {
+  .attr("y", d => {
     return d.y;
   })
-  .attr("width", function (d) {
+  .attr("width", d => {
     return barWidth;
   })
-  .attr("height", function (d) {
+  .attr("height", d => {
     return yScale(d.value);
   })
-  .style("fill", function (d) {
+  .style("fill", d => {
     return color(d.group);
   })
   .style("stroke", "none")
-  .attr("transform", function (d) {
+  .attr("transform", d => {
     return "rotate(" + d.a + "," + d.x + "," + d.y + ")"; // rotate the bar
   });
 
 // add date labels
-var tF = d3.timeFormat("%b %Y"),
+let tF = d3.timeFormat("%b %Y"),
   firstInMonth = {};
 
 svg.selectAll("text")
@@ -118,26 +118,26 @@ svg.selectAll("text")
   .style("font", "10px arial")
   .append("textPath")
   // only add for the first of each month
-  .filter(function (d) {
-    var sd = tF(d.date);
+  .filter(d => {
+    let sd = tF(d.date);
     if (!firstInMonth[sd]) {
       firstInMonth[sd] = 1;
       return true;
     }
     return false;
   })
-  .text(function (d) {
+  .text(d => {
     return tF(d.date);
   })
   // place text along spiral
   .attr("xlink:href", "#spiral")
   .style("fill", "grey")
-  .attr("startOffset", function (d) {
+  .attr("startOffset", d => {
     return ((d.linePer / spiralLength) * 100) + "%";
   })
 
 
-var tooltip = d3.select("#chart")
+let tooltip = d3.select("#chart")
   .append('div')
   .attr('class', 'tooltip');
 
@@ -147,7 +147,7 @@ tooltip.append('div')
   .attr('class', 'value');
 
 svg.selectAll("rect")
-  .on('mouseover', function (d) {
+  .on('mouseover', d => {
 
     tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
     tooltip.select('.value').html("Value: <b>" + Math.round(d.value * 100) / 100 + "<b>");
@@ -161,13 +161,13 @@ svg.selectAll("rect")
     tooltip.style('opacity', 2);
 
   })
-  .on('mousemove', function (d) {
+  .on('mousemove', d => {
     tooltip.style('top', (d3.event.layerY + 10) + 'px')
       .style('left', (d3.event.layerX - 25) + 'px');
   })
-  .on('mouseout', function (d) {
+  .on('mouseout', d => {
     d3.selectAll("rect")
-      .style("fill", function (d) {
+      .style("fill", d => {
         return color(d.group);
       })
       .style("stroke", "none")
